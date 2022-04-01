5a05a5e5f4be5f51ee1c49cdd5d8116045a25d79
soyou <ksoyoun95@naver.com>
2022-04-01 18:33:01 +0900

    calender ajax

--- soyeon/src/main/java/com/example/demo/controller/CalenderController.java
+++ soyeon/src/main/java/com/example/demo/controller/CalenderController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.controller.dao.CalenderRepository;
+import com.example.demo.dto.calender.entity.CalEntity;
 
 @Controller
 @RequestMapping(value = "/calender")
@@ -15,9 +22,44 @@ public class CalenderController {
 	@Autowired
 	CalenderRepository calRepository;
 	
-	@GetMapping("/")
-	public String calender(Model model){
+	@GetMapping("/calenderMain")
+	public String calender(HttpServletRequest request, Authentication authentication, Model model){
 		
-		return "/calender";
+	//	HttpSession session = request.getSession();
+		String id = authentication.getName();
+		System.out.println(id);
+		
+		model.addAttribute("id", id);
+		/*
+		CalEntity cal = CalEntity.builder()
+		.id(id)
+		.title("test")
+		.detail("detailTest")
+		.regDt(LocalDateTime.now())
+		.updateDt(LocalDateTime.now())
+		.build();
+		
+		calRepository.save(cal);
+		calRepository.findAllById(id);
+		
+		*/
+		
+		
+		
+		return "calender/calenderMain";
+	}
+	
+	@GetMapping("/calList")
+	public @ResponseBody List<CalEntity> calList(Authentication authentication, Model model) {
+		
+		String id = authentication.getName();
+		List<CalEntity> calList = calRepository.findAllById(id);
+		if(calList.size() > 0) {
+			model.addAttribute("calList", calList);
+		}else {
+			model.addAttribute("calList", "");
+		}
+		
+		return calList;
 	}
 }
--- soyeon/src/main/java/com/example/demo/controller/dao/CalenderRepository.java
+++ soyeon/src/main/java/com/example/demo/controller/dao/CalenderRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.dto.calender.entity.CalEntity;
 
 public interface CalenderRepository extends JpaRepository<CalEntity, Long>{
-
+	 List<CalEntity> findAllById(String id);
 }
